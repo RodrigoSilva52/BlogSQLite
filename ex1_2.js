74de974645581c96148f13de2bcaa7a218739c16
RodrigoSilva52 <elenarosangela15@gmail.com>
2025-03-14 13:27:38 -0300

    PORT

---
+++ ex1_2.js
@@ -0,0 +1,26 @@
+// Para usar o prompt no nodejs é preciso instalar essa lib 'prompt-sync'
+
+let prompt = require("prompt-sync");
+prompt = prompt();
+
+// 1.1. Cálculo de Desconto
+//  let preco = parseFloat(prompt("digite o preço:"));
+//  let desconto = parseFloat(prompt("digite o desconto:"));
+
+//  function calcularDesconto (preco, desconto){
+//      const resultado = preco -(preco * desconto/100);
+//      console.log("o valor com desconto é:", resultado);
+// }
+
+//  calcularDesconto(preco, desconto);
+
+//1.2. Conversor de Temperatura
+
+// let celsius = parseFloat(prompt("digite quantos graus está:"));
+
+// function celsiusParaFahrenheit(celsius) {
+//   const celsiusFahrenheit = (celsius * 9) / 5 + 32;
+//   console.log(celsiusFahrenheit);
+// }
+
+// conversao(celsius);
